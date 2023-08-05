62d1a5789901d6ad8cdf35dd31bb77087978e888
Navpreet Kaur <104396027+nkaur15@users.noreply.github.com>
2023-08-05 02:23:21 +0000

    git commit

---
+++ interactive.js
@@ -0,0 +1,9 @@
+//Trigger clicks
+<button id = "myButton">Click Me</button>
+
+<script>
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', {} +> {
+        alert("Button clicked!")
+    })
+</script>
