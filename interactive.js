ba5800f46f632e9535ac8595909f6150f9b94c36
Navpreet Kaur <104396027+nkaur15@users.noreply.github.com>
2023-08-14 02:53:52 +0000

    adding home page

--- interactive.js
+++ interactive.js
@@ -6,7 +6,7 @@ const resume = document.getElementById('Resume')
 
 home.addEventListener('click', () => {
     alert('You clicked home!');
-    window.open("https://nkaur15.github.io/Home")
+    ///window.open("https://nkaur15.github.io/Home")
 });
 
 projects.addEventListener('click', () => {
